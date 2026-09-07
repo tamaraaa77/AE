e0c9252587f610ed4058cba8dbd43c1568eabeb5
radablaa <radablagojevic6668@gmail.com>
2026-09-07 02:13:41 +0200

    manuelni rezim i LED bar

--- Starter/main_application.c
+++ Starter/main_application.c
@@ -69,12 +69,12 @@ SemaphoreHandle_t RXC_BinarySemaphore0;
 SemaphoreHandle_t RXC_BinarySemaphore1;
 SemaphoreHandle_t RXC_BinarySemaphore2;
 SemaphoreHandle_t Trigger_BinarySemaphore;
+SemaphoreHandle_t Blink_BinarySemaphore;
 
 QueueHandle_t Light_Queue;
 QueueHandle_t Door_Queue;
 
 TimerHandle_t per_TimerHandle;
-
 
 /* GLOBAL DATA */
 
@@ -89,23 +89,61 @@ static uint8_t current_mode = 0U;
 
 /* 0 = zatvorena, 1 = otvorena */
 static uint8_t door_state = 0U;
-
+static uint8_t blink_state = 0U;
+static TimerHandle_t blink_TimerHandle;
 
 /* RECEPTION BUFFERS */
 
 static uint8_t light_buffer[R_BUF_SIZE];
 static uint8_t door_buffer[R_BUF_SIZE];
 static uint8_t pc_buffer[R_BUF_SIZE];
-
 static uint8_t light_point = 0U;
 static uint8_t door_point = 0U;
 static uint8_t pc_point = 0U;
-
+static uint8_t drl_state = 0U;
+static uint8_t kratka_state = 0U;
 
 /* 7-SEGMENT */
 
 static const uint8_t hexnum[] = { 0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71 };
 
+
+typedef struct
+{
+    uint8_t broj_stuba;
+    uint8_t broj_diode;
+} LEDBarData;
+
+LEDBarData DRL_input;
+LEDBarData DRL_output;
+LEDBarData kratka_input;
+LEDBarData kratka_output;
+LEDBarData duga_input;
+LEDBarData duga_output;
+LEDBarData lijevi_input;
+LEDBarData lijevi_output;
+LEDBarData desni_input;
+LEDBarData desni_output;
+LEDBarData kabina_input;
+LEDBarData kabina_output;
+
+LEDBarData DRL_input = { 0U, 1U };
+LEDBarData DRL_output = { 1U, 8U };
+
+LEDBarData kratka_input = { 0U, 2U };
+LEDBarData kratka_output = { 1U, 7U };
+
+LEDBarData duga_input = { 0U, 3U };
+LEDBarData duga_output = { 1U, 6U };
+
+LEDBarData lijevi_input = { 0U, 4U };
+LEDBarData lijevi_output = { 1U, 5U };
+
+LEDBarData desni_input = { 0U, 5U };
+LEDBarData desni_output = { 1U, 4U };
+
+LEDBarData kabina_input = { 0U, 7U };
+LEDBarData kabina_output = { 1U, 1U };
 
 /* INTERRUPTS */
 
@@ -155,15 +193,40 @@ static void TimerCallback(TimerHandle_t xTimer)
     (void)xTimer;
     xSemaphoreGive(Trigger_BinarySemaphore);
 }
+static void BlinkTimerCallback(TimerHandle_t xTimer)
+{
+    (void)xTimer;
+
+    if (blink_state == 0U)
+    {
+        blink_state = 1U;
+    }
+    else
+    {
+        blink_state = 0U;
+    }
+
+    xSemaphoreGive(Blink_BinarySemaphore);
+}
 
 
 /* MAIN */
+static uint8_t LEDBar_DiodeToMask(uint8_t broj_diode)
+{
+    if ((broj_diode >= 1U) && (broj_diode <= 8U))
+    {
+        return (uint8_t)(1U << (broj_diode - 1U));
+    }
+
+    return 0U;
+}
 
 void main_demo(void)
 {
     /* PERIPHERALS */
 
     init_LED_comm();
+    set_LED_BAR(1U, 0U);
     init_7seg_comm();
 
     init_serial_uplink(COM_CH_0);
@@ -189,17 +252,14 @@ void main_demo(void)
     /* SEMAPHORES */
 
     LED_INT_BinarySemaphore = xSemaphoreCreateBinary();
-
     TBE_BinarySemaphore0 = xSemaphoreCreateBinary();
     TBE_BinarySemaphore1 = xSemaphoreCreateBinary();
     TBE_BinarySemaphore2 = xSemaphoreCreateBinary();
-
     RXC_BinarySemaphore0 = xSemaphoreCreateBinary();
     RXC_BinarySemaphore1 = xSemaphoreCreateBinary();
     RXC_BinarySemaphore2 = xSemaphoreCreateBinary();
-
     Trigger_BinarySemaphore = xSemaphoreCreateBinary();
-
+    Blink_BinarySemaphore = xSemaphoreCreateBinary();
 
     /* QUEUES */
 
@@ -213,13 +273,11 @@ void main_demo(void)
     if (TBE_BinarySemaphore0 == NULL) { while (1); }
     if (TBE_BinarySemaphore1 == NULL) { while (1); }
     if (TBE_BinarySemaphore2 == NULL) { while (1); }
-
     if (RXC_BinarySemaphore0 == NULL) { while (1); }
     if (RXC_BinarySemaphore1 == NULL) { while (1); }
     if (RXC_BinarySemaphore2 == NULL) { while (1); }
-
     if (Trigger_BinarySemaphore == NULL) { while (1); }
-
+    if (Blink_BinarySemaphore == NULL) { while (1); }
 
     /* CHECK QUEUES */
 
@@ -227,32 +285,34 @@ void main_demo(void)
     if (Door_Queue == NULL) { while (1); }
 
 
-    /* TIMER - 200 ms */
+    /* TAJMERI */
 
     per_TimerHandle = xTimerCreate("SensorTimer", pdMS_TO_TICKS(SENSOR_PERIOD_MS), pdTRUE, NULL, TimerCallback);
-
     if (per_TimerHandle == NULL) { while (1); }
-
     xTimerStart(per_TimerHandle, 0U);
+   
+    blink_TimerHandle = xTimerCreate("BlinkTimer", pdMS_TO_TICKS(500U), pdTRUE, NULL, BlinkTimerCallback);
+    if (blink_TimerHandle == NULL) { while (1); }
+    xTimerStart(blink_TimerHandle, 0U);
 
+    /* TASKOVI */
 
-    /* TASKS */
-
-    if (xTaskCreate(SensorLightReceive_Task, "LightRx", configMINIMAL_STACK_SIZE, NULL, TASK_SERIAL_REC_PRI, NULL) != pdPASS) { while (1); }
-
-    if (xTaskCreate(SensorDoorReceive_Task, "DoorRx", configMINIMAL_STACK_SIZE, NULL, TASK_SERIAL_REC_PRI, NULL) != pdPASS) { while (1); }
-
-    if (xTaskCreate(SensorTrigger_Task, "Trigger", configMINIMAL_STACK_SIZE, NULL, TASK_TRIGGER_PRI, NULL) != pdPASS) { while (1); }
-
-    if (xTaskCreate(PCReceive_Task, "PCRx", configMINIMAL_STACK_SIZE, NULL, TASK_PC_REC_PRI, NULL) != pdPASS) { while (1); }
-
-    if (xTaskCreate(PCSend_Task, "PCTx", configMINIMAL_STACK_SIZE, NULL, TASK_PC_SEND_PRI, NULL) != pdPASS) { while (1); }
-
-    if (xTaskCreate(DataProcessing_Task, "Processing", configMINIMAL_STACK_SIZE, NULL, TASK_DATA_PROC_PRI, NULL) != pdPASS) { while (1); }
-
-    if (xTaskCreate(LCDDisplay_Task, "Display", configMINIMAL_STACK_SIZE, NULL, TASK_LCD_PRI, NULL) != pdPASS) { while (1); }
-
-    if (xTaskCreate(LEDBar_Task, "LEDBar", configMINIMAL_STACK_SIZE, NULL, TASK_LED_PRI, NULL) != pdPASS) { while (1); }
+    if (xTaskCreate(SensorLightReceive_Task, "LightRx", configMINIMAL_STACK_SIZE, NULL, TASK_SERIAL_REC_PRI, NULL) != pdPASS)
+    { while (1); }
+    if (xTaskCreate(SensorDoorReceive_Task, "DoorRx", configMINIMAL_STACK_SIZE, NULL, TASK_SERIAL_REC_PRI, NULL) != pdPASS) 
+    { while (1); }
+    if (xTaskCreate(SensorTrigger_Task, "Trigger", configMINIMAL_STACK_SIZE, NULL, TASK_TRIGGER_PRI, NULL) != pdPASS)
+    { while (1); }
+    if (xTaskCreate(PCReceive_Task, "PCRx", configMINIMAL_STACK_SIZE, NULL, TASK_PC_REC_PRI, NULL) != pdPASS)
+    { while (1); }
+    if (xTaskCreate(PCSend_Task, "PCTx", configMINIMAL_STACK_SIZE, NULL, TASK_PC_SEND_PRI, NULL) != pdPASS)
+    { while (1); }
+    if (xTaskCreate(DataProcessing_Task, "Processing", configMINIMAL_STACK_SIZE, NULL, TASK_DATA_PROC_PRI, NULL) != pdPASS)
+    { while (1); }
+    if (xTaskCreate(LCDDisplay_Task, "Display", configMINIMAL_STACK_SIZE, NULL, TASK_LCD_PRI, NULL) != pdPASS) 
+    { while (1); }
+    if (xTaskCreate(LEDBar_Task, "LEDBar", configMINIMAL_STACK_SIZE, NULL, TASK_LED_PRI, NULL) != pdPASS) 
+    { while (1); }
 
 
     /* START SCHEDULER */
@@ -520,6 +580,19 @@ void DataProcessing_Task(void* pvParameters)
                 average_illumination = (uint16_t)(sum / sample_count);
                 printf("AVG: %u\n", (unsigned)average_illumination);
             }
+            if (current_mode == 0U)
+            {
+                if (average_illumination > illumination_threshold)
+                {
+                    drl_state = 1U;
+                    kratka_state = 0U;
+                }
+                else
+                {
+                    drl_state = 0U;
+                    kratka_state = 1U;
+                }
+            }
         }
 
         if (xQueueReceive(Door_Queue, &new_door_state, 0U) == pdPASS)
@@ -575,17 +648,95 @@ void LCDDisplay_Task(void* pvParameters)
 
 
 /* LED BAR TASK */
-
 void LEDBar_Task(void* pvParameters)
 {
     uint8_t d = 0U;
+    uint8_t output = 0U;
 
     (void)pvParameters;
 
     while (1)
     {
-        xSemaphoreTake(LED_INT_BinarySemaphore, portMAX_DELAY);
-        get_LED_BAR(1, &d);
-        printf("LED BAR INPUT: %u\n", (unsigned)d);
+        (void)xSemaphoreTake(LED_INT_BinarySemaphore, 0U);
+        (void)xSemaphoreTake(Blink_BinarySemaphore, 0U);
+
+        (void)get_LED_BAR(DRL_input.broj_stuba, &d);
+        output = 0U;
+
+        if (current_mode == 1U)
+        {
+            if ((d & LEDBar_DiodeToMask(DRL_input.broj_diode)) != 0U)
+            {
+                output |= LEDBar_DiodeToMask(DRL_output.broj_diode);
+            }
+
+            if ((d & LEDBar_DiodeToMask(kratka_input.broj_diode)) != 0U)
+            {
+                output |= LEDBar_DiodeToMask(kratka_output.broj_diode);
+            }
+
+            if ((d & LEDBar_DiodeToMask(duga_input.broj_diode)) != 0U)
+            {
+                output |= LEDBar_DiodeToMask(duga_output.broj_diode);
+            }
+
+            if ((d & LEDBar_DiodeToMask(lijevi_input.broj_diode)) != 0U)
+            {
+                if (blink_state != 0U)
+                {
+                    output |= LEDBar_DiodeToMask(lijevi_output.broj_diode);
+                }
+            }
+
+            if ((d & LEDBar_DiodeToMask(desni_input.broj_diode)) != 0U)
+            {
+                if (blink_state != 0U)
+                {
+                    output |= LEDBar_DiodeToMask(desni_output.broj_diode);
+                }
+            }
+
+            if ((d & LEDBar_DiodeToMask(kabina_input.broj_diode)) != 0U)
+            {
+                output |= LEDBar_DiodeToMask(kabina_output.broj_diode);
+            }
+        }
+        else
+        {
+            if (drl_state != 0U)
+            {
+                output |= LEDBar_DiodeToMask(DRL_output.broj_diode);
+            }
+
+            if (kratka_state != 0U)
+            {
+                output |= LEDBar_DiodeToMask(kratka_output.broj_diode);
+            }
+
+            if ((d & LEDBar_DiodeToMask(duga_input.broj_diode)) != 0U)
+            {
+                output |= LEDBar_DiodeToMask(duga_output.broj_diode);
+            }
+
+            if ((d & LEDBar_DiodeToMask(lijevi_input.broj_diode)) != 0U)
+            {
+                if (blink_state != 0U)
+                {
+                    output |= LEDBar_DiodeToMask(lijevi_output.broj_diode);
+                }
+            }
+
+            if ((d & LEDBar_DiodeToMask(desni_input.broj_diode)) != 0U)
+            {
+                if (blink_state != 0U)
+                {
+                    output |= LEDBar_DiodeToMask(desni_output.broj_diode);
+                }
+            }
+        }
+
+        (void)set_LED_BAR(DRL_output.broj_stuba, output);
+
+        vTaskDelay(pdMS_TO_TICKS(10U));
     }
 }
